3c58ed992aa6c7855ea2098fb3fd98523918932f
Alexjetx <krasnobaev348@gmail.com>
2024-10-04 00:35:26 +0300

    Add files via upload

---
+++ AviatorAdmin/static/Aviator/main.js
@@ -0,0 +1,48 @@
+const getSignal = document.getElementById("get-signal");
+const stopSignalTimeBlock = document.getElementById("stop-signal-time-block");
+const printSignal = document.getElementById("print-signal");
+const stopProgress = document.getElementById("stop-progress");
+
+const coefficients = [2.0, 3.5, 5.2]; // Массив с коэффициентами
+let currentIndex = 0;
+
+
+getSignal.onclick = async function () {
+  let receivingSignal = coefficients[currentIndex];
+
+  // Увеличиваем индекс для следующего нажатия
+  currentIndex = (currentIndex + 1) % coefficients.length; // Циклический переход
+
+  // Отображаем коэффициент
+  printSignal.innerHTML = `${receivingSignal}${"x"}`;
+  printSignal.classList.remove("deactivate");
+
+  // Запускаем таймер
+  goTimer(20);
+
+  // Деактивируем кнопку
+  getSignal.disabled = true;
+}
+
+// Таймер после получения сигнала
+function goTimer(time) {
+  const timer = setInterval(() => {
+    if (time >= 1) {
+      getSignal.classList.add("deactivate");
+      getSignal.style["z-index"] = "-1";
+      stopProgress.style["animation"] = "animateProgress 20s linear infinite";
+      stopSignalTimeBlock.classList.remove("deactivate");
+      let stopTimer = document.getElementById("stop-timer");
+      stopTimer.innerHTML = `${time--}${"<span> seconds</span>"}`;
+      timerr = time;
+      getSignal.disabled = true;
+    } else {
+      getSignal.classList.remove("deactivate");
+      getSignal.style["z-index"] = "5";
+      stopSignalTimeBlock.classList.add("deactivate");
+      stopProgress.style["animation"] = "none";
+      clearInterval(timer);
+      getSignal.disabled = false;
+    }
+  }, 1000)
+}
