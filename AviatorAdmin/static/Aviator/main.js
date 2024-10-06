f95f2f58eae6b64ef046933369dd678f13050b91
Alexjetx <krasnobaev348@gmail.com>
2024-10-06 15:59:54 +0200

    Update main.js

--- AviatorAdmin/static/Aviator/main.js
+++ AviatorAdmin/static/Aviator/main.js
@@ -3,7 +3,7 @@ const stopSignalTimeBlock = document.getElementById("stop-signal-time-block");
 const printSignal = document.getElementById("print-signal");
 const stopProgress = document.getElementById("stop-progress");
 
-const coefficients = [2.69, 2.69, 2.69, 2.69, 2.69, 2.69, 2.69, 2.69]; // Массив с коэффициентами
+const coefficients = [2.57, 2.57, 2.57, 2.57, 2.57, 2.58, 2.57, 2.57]; // Массив с коэффициентами
 let currentIndex = 0;
 
 
